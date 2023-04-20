fe3523d3c29f2fa7685e85363fc095c0c76b805b
Zilan Zaman <aazilanzaman@gmail.com>
2023-04-20 14:51:06 +0600

    second commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,11 +8,13 @@ const Portfolio = () => {
       id: 1,
       src: HearHub,
       link: "https://hear-hub-a-ecommerce-website.vercel.app/" ,
+      code:"https://github.com/Tahsin-Zaman-Jilan/HearHub-A-Ecommerce-Website" ,
     },
     {
       id: 2,
       src: portfolio,
-      link:"",
+      link:"https://jilanportfolio.netlify.app/",
+      code:"https://github.com/Tahsin-Zaman-Jilan/PortFolio-Website",
     },
     
   ];
@@ -31,7 +33,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src,link }) => (
+          {portfolios.map(({ id, src,link ,code}) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -39,13 +41,13 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-              <a href="https://hear-hub-a-ecommerce-website.vercel.app/" target="_blank" rel="noreferrer" >
+              <a href={link} target="_blank" rel="noreferrer" >
   <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
     Live
   </button>
 </a>
 
-<a href="https://github.com/Tahsin-Zaman-Jilan/HearHub-A-Ecommerce-Website" target="_blank" rel="noreferrer" >
+<a href={code}  target="_blank" rel="noreferrer" >
   <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
     Code
   </button>
